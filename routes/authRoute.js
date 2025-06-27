eccdf1a93b6b9552f2b8d248af7eceea9c24c06b
Rajankumar001 <chaudharyrajan387@gmail.com>
2025-06-27 13:12:00 +0530

    auth controller added

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -4,7 +4,10 @@ const router = express.Router()
 import {
   registerController,
   loginController,
-  forgotPasswordController
+  forgotPasswordController,
+  updateProfileController,
+  getOrdersController,
+  getAllOrdersController
 } from "../controllers/authController.js";
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 router.post("/register", registerController);
@@ -20,5 +23,22 @@ router.get("/user-auth", requireSignIn, (req, res) => {
 router.get("/admin-auth", requireSignIn,isAdmin, (req, res) => {
   res.status(200).send({ ok: true });
 });
+//update profile
+router.put("/profile", requireSignIn, updateProfileController);
+
+//orders
+router.get("/orders", requireSignIn, getOrdersController);
+
+//all orders
+router.get("/all-orders", requireSignIn, isAdmin, getAllOrdersController);
+
+// order status update
+// router.put(
+//   "/order-status/:orderId",
+//   requireSignIn,
+//   isAdmin,
+//   orderStatusController
+// );
+
 
 export default router;
